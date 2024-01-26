2f2675704a85d975f7d5cd65c15a47d0876aa547
Amalia <amalia@pop-os.localdomain>
2024-01-26 17:10:42 +0100

    test it starts from the top middle

--- part2/tetris/src/Board.mjs
+++ part2/tetris/src/Board.mjs
@@ -1,21 +1,30 @@
 export class Board {
   width;
   height;
+  pos;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.pos = [-1, -1]
   }
 
   toString() {
     let board = "";
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        board += ".";
+        if (x == this.pos[0] && y == this.pos[1]) {
+          board += "X"
+        } else {
+          board += ".";
+        }
       }
       board += "\n"
     }
     return board;
   }
 
+  drop(shape) {
+    this.pos = [1, 0]
+  }
 }
