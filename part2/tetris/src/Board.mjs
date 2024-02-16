db91d5a6561855820022f02b1c063da2d270974d
Amalia <amalia@pop-os.localdomain>
2024-02-16 14:45:23 +0100

    When a block is dropped

--- part2/tetris/src/Board.mjs
+++ part2/tetris/src/Board.mjs
@@ -1,20 +1,28 @@
 export class Board {
   width;
   height;
+  cutBlock;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.curBlock = [];
   }
 
   toString() {
     let board = "";
     for (let y = this.height - 1; y >= 0; y--) {
       for (let x = 0; x < this.width; x++) {
-        board += ".";
+        if (this.curBlock[0] == x && this.curBlock[1] == y) {
+          board += "X"
+        } else {board += ".";}
       }
       board += "\n";
     }
     return board;
+  }
+
+  drop(shape) {
+    this.curBlock = [(this.width - 1) / 2, this.height - 1]
   }
 }
