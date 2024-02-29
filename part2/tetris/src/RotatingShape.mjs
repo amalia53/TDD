6f91b68556538eecf18bcd6c2b6a70e7fe693052
Amalia <amalia@pop-os.localdomain>
2024-02-29 16:12:40 +0100

    refactoring

--- part2/tetris/src/RotatingShape.mjs
+++ part2/tetris/src/RotatingShape.mjs
@@ -1,26 +1,32 @@
 export class RotatingShape {
 
-  line1;
-  line2;
-  line3;
   shape;
 
   constructor(shape) {
-    let lines = shape.split('\n');
-    this.line1 = lines[0].trim();
-    this.line2 = lines[1].trim();
-    this.line3 = lines[2].trim();
-    this.shape = this.line1  + '\n' + this.line2 + '\n' + this.line3 + '\n';
+    const lines = shape.split('\n');
+    const line1 = lines[0].trim();
+    const line2 = lines[1].trim();
+    const line3 = lines[2].trim();
+    this.shape = [[line1[0], line1[1], line1[2]],
+    [line2[0], line2[1], line2[2]],
+    [line3[0], line3[1], line3[2]]];
   }
 
   toString() {
-    return this.shape;
+    let shapeToString = "";
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        shapeToString += this.shape[y][x]
+      }
+      shapeToString += '\n';
+    }
+    return shapeToString;
   }
 
   rotateRight() {
-    const newLine1 = this.line3[0] + this.line2[0] + this.line1[0];
-    const newLine2 = this.line3[1] + this.line2[1] + this.line1[1];
-    const newLine3 = this.line3[2] + this.line2[2] + this.line1[2];
+    const newLine1 = this.shape[2][0] + this.shape[1][0] + this.shape[0][0];
+    const newLine2 = this.shape[2][1] + this.shape[1][1] + this.shape[0][1];
+    const newLine3 = this.shape[2][2] + this.shape[1][2] + this.shape[0][2];
     return newLine1 + '\n' + newLine2 + '\n' + newLine3 + '\n';
   }
 }
