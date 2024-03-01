4e96a621700afb6e1b73cec988748dbe077c3e46
Amalia <amalia@pop-os.localdomain>
2024-03-01 13:26:52 +0100

    The T shape initial orientation

--- part2/tetris/src/Tetromino.mjs
+++ part2/tetris/src/Tetromino.mjs
@@ -1,14 +1,9 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 export class Tetromino {
 
-static T_SHAPE =
-  `.T.
+  static T_SHAPE =
+    `.T.
 TTT
 ...
 `
-
-constructor() {}
-
-toString() {return T_SHAPE}
-
 }
