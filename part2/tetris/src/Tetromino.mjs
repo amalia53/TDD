31a4a1749499c6de1079b1f868e5565f2119d63e
Amalia <amalia@pop-os.localdomain>
2024-03-19 14:11:21 +0100

    can be rotated right/clockwise

--- part2/tetris/src/Tetromino.mjs
+++ part2/tetris/src/Tetromino.mjs
@@ -1,14 +1,18 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 export class Tetromino {
 
-  static T_SHAPE = new Tetromino(`.T.\nTTT\n...\n`);
+  static T_SHAPE = new Tetromino(`.T.\nTTT\n...\n`, 4);
 
-  static I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....\n`);
+  static I_SHAPE = new Tetromino(`.....\n.....\nIIII.\n.....\n.....\n`, 2);
 
   shape;
+  orientations;
+  curOrientatino;
 
-  constructor(shape) {
+  constructor(shape, orientations) {
     this.shape = shape.substring(0, shape.length - 1);
+    const piece = new RotatingShape(shape)
+    this.orientations = [piece, piece.rotateRight(), piece.rotateRight().rotateRight(), piece.rotateLeft()].slice(0, orientations);
   }
 
   toString() {
